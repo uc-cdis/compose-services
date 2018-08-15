b8b93e9d041f39b00519f04b4913fbd99c5b8886
Steven Cui <scui729@uchicago.edu>
2018-08-15 11:48:38 -0500

    fix(startup_scripts): added postgres check for fence, peregrine, and sheepdog

--- scripts/fence_setup.sh
+++ scripts/fence_setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # entrypoint script for fence to sync user.yaml before running
 
+sleep 2
+until (echo > /dev/tcp/postgres/5432) >/dev/null 2>&1; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
+echo "postgres is ready"
+
 fence-create sync --yaml user.yaml
 
 rm -f /var/run/apache2/apache2.pid && /usr/sbin/apache2ctl -D FOREGROUND
--- scripts/sheepdog_setup.sh
+++ scripts/sheepdog_setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # entrypoint script for sheepdog to run setup_transactionlogs.py before running
 
+sleep 2
+until (echo > /dev/tcp/postgres/5432) >/dev/null 2>&1; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
+echo "postgres is ready"
+
 python /sheepdog/bin/setup_transactionlogs.py --host postgres --user sheepdog_user --password sheepdog_pass --database metadata_db
 bash /sheepdog/dockerrun.bash
