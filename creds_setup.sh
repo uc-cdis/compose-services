74e0e6dbfa661b6d06701872685f97223cc25836
Reuben Pasquini <reuben@frickjack.com>
2019-03-05 16:10:25 -0600

    fix(creds_setup.sh): allow multiple runs

--- creds_setup.sh
+++ creds_setup.sh
@@ -1,72 +1,100 @@
 #!/bin/bash
 # Script to setup keys for fence as well as ssl credentials 
 
-if [[ -e ./Secrets ]]; then
-  echo "ERROR: Secrets/ folder already exists - bailing out"
+if [[ ! -d ./templates ]]; then
+  echo "ERROR: ./templates not found - run in compose-services folder"
   exit 1
 fi
-
-if [[ ! -d ./apis_configs ]]; then
-  echo "ERROR: ./apis_configs not found - run in compose-services folder"
-  exit 1
+if [[ -d Secrets ]]; then
+  # make a backup
+  bak="Secrets$(date +%Y%m%d%H%M%S).bak"
+  if [[ -e "$bak" ]]; then
+    echo "ERROR: ./Secrets and $bak already exist"
+    exit 1
+  fi
+  echo "Backing up ./Secrets/ to ./$bak/"
+  cp -r ./Secrets "./$bak"
 fi
 
-cp -r ./apis_configs ./Secrets
+mkdir -p Secrets
+
+for path in templates/*; do
+  target="Secrets/$(basename "$path")"
+  if [[ "$path" =~ \.py$ ]]; then # update python files
+    echo "Copying $path to $target"
+    cp "$path" "$target"
+  elif [[ ! -e "$target" ]]; then
+    echo "Copying $path to $target"
+    cp -r "$path" "$target"
+  else
+    echo "$target already exists"
+  fi
+done
+
 cd Secrets
 
 # make directories for temporary credentials
 timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-mkdir -p TLS
-mkdir -p fenceJwtKeys
-mkdir -p fenceJwtKeys/${timestamp}
 
 # generate private and public key for fence
-openssl genpkey -algorithm RSA -out fenceJwtKeys/${timestamp}/jwt_private_key.pem \
-    -pkeyopt rsa_keygen_bits:2048
-openssl rsa -pubout -in fenceJwtKeys/${timestamp}/jwt_private_key.pem \
-    -out fenceJwtKeys/${timestamp}/jwt_public_key.pem
-chmod -R a+rx fenceJwtKeys
+yearMonth="$(date +%Y-%m)"
+if [[ ! -d ./fenceJwtKeys ]] || ! (ls ./fenceJwtKeys | grep "$yearMonth" > /dev/null 2>&1); then
+    echo "Generating fence OAUTH key pairs under Secrets/fenceJwtKeys"
+    mkdir -p fenceJwtKeys
+    mkdir -p fenceJwtKeys/${timestamp}
 
-OS=$(uname)
-OPTS=""
-if [[ $OS == "Darwin" ]]; then
-    cp /etc/ssl/openssl.cnf openssl-with-ca.cnf
+    openssl genpkey -algorithm RSA -out fenceJwtKeys/${timestamp}/jwt_private_key.pem \
+        -pkeyopt rsa_keygen_bits:2048
+    openssl rsa -pubout -in fenceJwtKeys/${timestamp}/jwt_private_key.pem \
+        -out fenceJwtKeys/${timestamp}/jwt_public_key.pem
+    chmod -R a+rx fenceJwtKeys
+fi
 
-    __v3_ca="
+# generate certs for nginx ssl
+(
+    mkdir -p TLS
+    cd TLS
+
+    OS=$(uname)
+    OPTS=""
+    if [[ $OS == "Darwin" ]]; then
+        cp /etc/ssl/openssl.cnf openssl-with-ca.cnf
+
+        __v3_ca="
 [ v3_ca ]
 basicConstraints = critical,CA:TRUE
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer:always
 "
+        echo "$__v3_ca" >> openssl-with-ca.cnf
+        OPTS=" -extensions v3_ca -config openssl-with-ca.cnf"
+    fi
 
-    echo "$__v3_ca" >> openssl-with-ca.cnf
-    OPTS=" -extensions v3_ca -config openssl-with-ca.cnf"
-fi
+    if ! [[ -f openssl.cnf && -f ca.pem && -f ca-key.pem ]]; then
+      echo "Generating a local certificate authority, and TLS certificates under Secrets/TLS/"
+      # erase old certs if they exist
+      /bin/rm -rf service.key service.crt
+      commonName=${1:-localhost}
+      SUBJ="/countryName=US/stateOrProvinceName=IL/localityName=Chicago/organizationName=CDIS/organizationalUnitName=PlanX/commonName=$commonName/emailAddress=cdis@uchicago.edu"
+      openssl req -new -x509 -nodes -extensions v3_ca -keyout ca-key.pem \
+          -out ca.pem -days 365 -subj $SUBJ $OPTS
+      if [[ $? -eq 1 ]]; then    
+          echo "problem with creds_setup.sh script, refer to compose-services wiki"
+          rm -rf temp*
+          exit 1
+      fi
 
-# generate certs for nginx ssl
-commonName=${1:-localhost}
-SUBJ="/countryName=US/stateOrProvinceName=IL/localityName=Chicago/organizationName=CDIS/organizationalUnitName=PlanX/commonName=$commonName/emailAddress=cdis@uchicago.edu"
-openssl req -new -x509 -nodes -extensions v3_ca -keyout TLS/ca-key.pem \
-    -out TLS/ca.pem -days 365 -subj $SUBJ $OPTS
-if [[ $? -eq 1 ]]; then    
-    echo "problem with creds_setup.sh script, refer to compose-services wiki"
-    rm -rf temp*
-    exit 1
-fi
-
-
-(
-    cd TLS
-    mkdir -p CA/newcerts
-    touch CA/index.txt
-    echo 1000 > CA/serial
-    cat > openssl.cnf <<EOM
+      mkdir -p CA/newcerts
+      touch CA/index.txt
+      touch CA/index.txt.attr
+      echo 1000 > CA/serial
+      cat > openssl.cnf <<EOM
 [ ca ]
 # man ca
 default_ca = CA_default
 [ CA_default ]
 # Directory and file locations.
-dir             = TLS              # Where everything is kept
+dir             = .                      # Where everything is kept
 new_certs_dir   = \$dir/CA/newcerts
 database        = \$dir/CA/index.txt     # database index file.
 certificate     = \$dir/ca.pem           # The CA certificate
@@ -98,10 +126,16 @@ extendedKeyUsage = serverAuth
 # Extension for CRLs ('man x509v3_config').
 authorityKeyIdentifier=keyid:always
 EOM
-
-  )
-openssl genrsa -out "TLS/service.key" 2048
-openssl req -new -key "TLS/service.key" \
-    -out "TLS/service.csr" -subj $SUBJ
-openssl ca -batch -in "TLS/service.csr" -config TLS/openssl.cnf \
-    -extensions server_cert -days 365 -notext -out "TLS/service.crt" 
+    else
+      echo "Looks like Secrets/TLS/CA already exists"
+    fi
+    if [[ ! -f service.key || ! -f service.crt ]]; then
+      openssl genrsa -out "service.key" 2048
+      openssl req -new -key "service.key" \
+          -out "service.csr" -subj $SUBJ
+      openssl ca -batch -in "service.csr" -config openssl.cnf \
+          -extensions server_cert -days 365 -notext -out "service.crt" 
+    else
+      echo "Looks like Secrets/TLS/service.key and service.crt already exist"
+    fi
+)
