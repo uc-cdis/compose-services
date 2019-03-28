723c70accbda4fc504a929246f2ca004553ea5a3
vpsx <19900057+vpsx@users.noreply.github.com>
2019-03-28 17:46:43 -0500

    Change example dictionary (#34)
    * Change example dictionary
    
    * Remove pyyaml and jsonschema from setup.py

---
+++ datadictionary/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gdcdictionary',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'dictionaryutils',
+    ],
+    dependency_links=[
+       "git+https://github.com/uc-cdis/dictionaryutils.git@2.0.4#egg=dictionaryutils",
+    ],
+    package_data={
+        "gdcdictionary": [
+            "schemas/*.yaml",
+            "schemas/projects/*.yaml",
+            "schemas/projects/*/*.yaml",
+        ]
+    },
+)
