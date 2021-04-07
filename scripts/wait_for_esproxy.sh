454d06358a49b4455097e34ddc060e76903e1aa3
Victor San Kho Lin <victor@sankholin.com>
2021-04-07 14:16:18 +1000

    Fixed tube etl mapping
    * Updated etl mapping and gitops to support tube node id
      naming convention using underscore. See
      https://github.com/uc-cdis/tube/pull/131
    * Allowed ES cluster in either `green` or `yellow` mode
    * Added `esdata` volume for ES persistent
    * Updated Fence `user.yaml` template to align with common
      use cases as written guide in README
    * Updated tip and workaround in README
    * Featuring `docker-compose.override.yml` to keep the base
      docker compose config clean

--- scripts/wait_for_esproxy.sh
+++ scripts/wait_for_esproxy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-until curl -f -s http://esproxy-service:9200/_cluster/health | python3 -c "import sys, json; sys.exit(0 if json.load(sys.stdin)['status'] == 'green' else 1)" 2>/dev/null;
+until curl -f -s http://esproxy-service:9200/_cluster/health | python3 -c "import sys, json; sys.exit(0 if json.load(sys.stdin)['status'] != 'red' else 1)" 2>/dev/null;
 do
   echo "esproxy not ready, waiting..."
   sleep 5
