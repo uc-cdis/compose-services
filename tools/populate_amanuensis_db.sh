c80649189eb0d23b4d3becb9e27c6e6a8b00beae
grugna <graglia01@gmail.com>
2022-03-21 11:48:41 -0500

    add more fake amanuensis data

--- tools/populate_amanuensis_db.sh
+++ tools/populate_amanuensis_db.sh
@@ -16,12 +16,16 @@ curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACC
 
 
 echo "Adding Search in Amanuensis DB"
-curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"user_id": '"$USER_ID"', "name": "inrg-test", "description": "admin creates a search list for a user", "ids_list": ["COG_0xA4CE42BAEAFFD85A5A573F7C0488647D","COG_0x2B1D2E3C4648236211D982AA60BAC9BD", "COG_0xE23B0F16F4B158D1A417B2B422AEB303"]}' http://localhost/amanuensis/admin/filter-sets
-
+curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"user_id": '"$USER_ID"', "name": "inrg-test-1", "description": "admin creates a search list for a user", "ids_list": ["COG_0xA4CE42BAEAFFD85A5A573F7C0488647D","COG_0x2B1D2E3C4648236211D982AA60BAC9BD", "COG_0xE23B0F16F4B158D1A417B2B422AEB303"]}' http://localhost/amanuensis/admin/filter-sets
+curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"user_id": '"$USER_ID"', "name": "inrg-test-2", "description": "admin creates a search list for a user", "ids_list": ["COG_0xA4CE42BAEAFFD85A5A573F7C0488647D","COG_0x2B1D2E3C4648236211D982AA60BAC9BD", "COG_0xE23B0F16F4B158D1A417B2B422AEB303"]}' http://localhost/amanuensis/admin/filter-sets
+curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"user_id": '"$USER_ID"', "name": "inrg-test-3", "description": "admin creates a search list for a user", "ids_list": ["COG_0xA4CE42BAEAFFD85A5A573F7C0488647D","COG_0x2B1D2E3C4648236211D982AA60BAC9BD", "COG_0xE23B0F16F4B158D1A417B2B422AEB303"]}' http://localhost/amanuensis/admin/filter-sets
 
 echo "Adding Project in Amanuensis DB"
-curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"user_id": '"$USER_ID"', "name": "inrg-test-req1", "description": "inrg-test-req description", "filter_set_ids": [1], "institution": "UChicago"}' http://localhost/amanuensis/admin/projects
-
+curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"user_id": '"$USER_ID"', "name": "inrg-test-req1", "description": "inrg-test-req description", "filter_set_ids": [1,2], "institution": "UChicago"}' http://localhost/amanuensis/admin/projects
+curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"user_id": '"$USER_ID"', "name": "inrg-test-req1", "description": "inrg-test-req description", "filter_set_ids": [3], "institution": "UChicago"}' http://localhost/amanuensis/admin/projects
 
 echo "Changing data request state"
-curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"project_id": 1, "state_id": 1}' http://localhost/amanuensis/admin/projects/state
+curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"project_id": 1, "state_id": 3}' http://localhost/amanuensis/admin/projects/state
+
+echo "Adding download URL for approved data request"
+curl -X POST  -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -d '{"project_id": 1, "approved_url": "https://pcdc-gen3-dictionaries.s3.amazonaws.com/pcdc-schema-prod-20220106.json"}' http://localhost/amanuensis/admin/projects/state
